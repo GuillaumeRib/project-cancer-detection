78a68706c5ba41cac3067817b355f25f1ae1350a
Guillaume Ribiere <gribiere@gmail.com>
2022-09-10 09:02:34 +0200

    change in the code to flip from data_source

--- project_cancer_detection/interface/main_local.py
+++ project_cancer_detection/interface/main_local.py
@@ -1,7 +1,24 @@
+from webbrowser import get
 from tensorflow.keras.callbacks import EarlyStopping
 from project_cancer_detection.ml_logic.initialize_model import init_model
 from project_cancer_detection.ml_logic.preprocessor import preprocessed
 import os
+
+
+def get_paths():
+    DATA_SOURCE = os.environ.get("DATA_SOURCE")
+
+    if DATA_SOURCE == 'local':
+        train_path = os.environ.get('LOCAL_TRAIN_PATH')
+        test_path = os.environ.get('LOCAL_TEST_PATH')
+
+    if DATA_SOURCE == 'cloud':
+        train_path = os.environ.get('CLOUD_TRAIN_PATH')
+        test_path = os.environ.get('CLOUD_TEST_PATH')
+
+    print(f'### Sourcing data from {DATA_SOURCE} ... ###\n')
+    return train_path,test_path
+
 
 def get_history(train_generator, val_generator):
     model = init_model()
@@ -25,13 +42,22 @@ def evaluate_model_04():
 
 if __name__ == '__main__':
     # Link to your sample train_path (manually selected for now)
+    '''
     local_train_path = os.environ['LOCAL_TRAIN_PATH']
     local_test_path = os.environ['LOCAL_TEST_PATH']
     cloud_train_path = os.environ['CLOUD_TRAIN_PATH']
     cloud_test_path = os.environ['CLOUD_TEST_PATH']
+    '''
 
-    print("1")
-    train_generator, val_generator, test_generator = preprocessed(cloud_train_path, cloud_test_path)
+    train_path, test_path = get_paths()
+
+    print('### Preprocessing & generators starting ... ###')
+    train_generator, val_generator, test_generator = preprocessed(train_path, test_path)
+    print('### Preprocessing & generators done! ###\n')
+
+    print('### Model fit starting ... ###')
     history, model = get_history(train_generator, val_generator)
+    print('### Model fit done ! Starting evaluation ... ###\n')
+
     evaluate_model(model, test_generator)
     #evaluate_model_04()
